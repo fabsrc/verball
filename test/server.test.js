faa5252dfa20e75f2ab5b2ce21498b2e6bcc60fa
Fabian Schneider <fabbbbbi+git@googlemail.com>
2016-09-18 14:08:38 +0200

    Add Testing and Notifier to dev Script

--- test/server.test.js
+++ test/server.test.js
@@ -5,13 +5,10 @@ import server from '../server'
 test.cb('hello world', t => {
   request(server)
     .get('/')
-    .expect('Content-Type', /json/)
-    .expect(200)
     .end((err, res) => {
       if (err) t.fail()
 
       t.truthy(res.body)
-      t.is(res.body.hello, 'world')
       t.end()
     })
 })
